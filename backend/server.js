2a5b696928fb29a1d0e40dbc642a931a9cfbf156
Tom Barnes <barnes.thomas.c@gmail.com>
2018-12-21 14:25:31 -0500

    added update and delete routes

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,34 @@ router.post('/comments', (req, res) => {
     });
 });
 
+router.put('/comments/:commentId', (req, res) => {
+    const { commentId } = req.params;
+    if (!commentId) {
+        return res.json({ success: false, error: 'No comment id provided' });
+    }
+    Comment.findById(commentId, (error, comment) => {
+        if (error) return res.json({success: false, error });
+        const { author, text } = req.body;
+        if (author) comment.author = author;
+        if (text) comment.text = text;
+        comment.save(error => {
+            if (error) return res.json({success: false, error});
+            return res.json({ success: true });
+        });
+    });
+});
+
+
+router.delete('/comments/:commentId', (req, res) => {
+    const { commentId } = req.params;
+    if (!commentId) {
+        return res.json({ success: false, error: 'No comment id provided' });
+    }
+    Comment.remove({ _id: commentId }, (error, comment) => {
+        if (error) return res.json({ success: false, error });
+        return res.json({ success: true });
+    });
+});
 
 // Use our router configuration when we call /api
 app.use('/api', router);
